8eea452c592898ea188306c32ecdb7a1bec022d5
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 18:35:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,12 @@ require 'pry'
 def square_array(array)
   numbers = []
   array.each do |number|
+<<<<<<< HEAD
    numbers << number**2
   end
   numbers
+=======
+    number**2
+  end
+>>>>>>> 80d90a8c85d3cc510e7c94f95f6592d9b3c4357a
 end
